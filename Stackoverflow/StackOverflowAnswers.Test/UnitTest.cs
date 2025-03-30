86876738a06d810094ba73d806ee137294f21929
Morten Bork <0robert.monk0@gmail.com>
2025-03-30 16:09:16 +0200

    First stack  answer. How to merge PFD's

---
+++ Stackoverflow/StackOverflowAnswers.Test/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using iTextSharp.text.log;
+using Microsoft.Extensions.Logging;
+using Moq;
+using StackoverflowAnswers;
+
+namespace StackOverflowAnswers.Test;
+
+public class StackOverflowAnswersTests
+{
+    [Fact]
+    public void TestAnswer()
+    {
+        Mock<ILogger<PdfStreamsHandler>> mock = new Mock<ILogger<PdfStreamsHandler>>();
+        PdfStreamsHandler pdfStreamHandler = new PdfStreamsHandler(mock.Object);
+        MemoryStream actualResult = pdfStreamHandler.MergePdfs();
+        Assert.NotNull(actualResult);
+    }
+}
+
